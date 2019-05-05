4e4da22b383187277ad533b32e3750b39e10d959
juaanhs <ruaanhs@gmail.com>
2019-05-05 12:36:13 -0300

    v01

---
+++ app/src/main/java/br/com/juaanhs/agenda/FormularioActivity.java
@@ -0,0 +1,63 @@
+package br.com.juaanhs.agenda;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import br.com.juaanhs.agenda.dao.AlunoDAO;
+import br.com.juaanhs.agenda.modelo.Aluno;
+
+public class FormularioActivity extends AppCompatActivity {
+
+    private FormularioHelper helper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+
+        helper = new FormularioHelper(this);
+
+        Intent intent = getIntent();
+        Aluno aluno = (Aluno) intent.getSerializableExtra("aluno");
+
+        if(aluno != null) {
+            helper.preencheFormulario(aluno);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) { //ITENS DO MENU DA BARRA SUPERIOR DO ANDROID
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_formulario, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+
+            case R.id.menu_formulario_ok:
+                Aluno aluno = helper.pegaAluno();
+                AlunoDAO dao = new AlunoDAO(this);
+
+                if(aluno.getId() != null){
+                    dao.altera(aluno);
+                } else {
+                    dao.insere(aluno);
+                }
+                dao.close();
+
+                Toast.makeText(FormularioActivity.this, "Aluno "+ aluno.getNome() +" salvo!", Toast.LENGTH_SHORT).show();
+
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
+
